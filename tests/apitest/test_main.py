43ecc98cedead74a813ffdd55699f2c318096329
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2024-10-01 09:30:13 +0100

    Added a simple api test

---
+++ tests/apitest/test_main.py
@@ -0,0 +1,21 @@
+
+# PyTest imports
+import pytest
+
+# Project specific imports
+from fastapi.testclient import TestClient
+
+# Package imports
+from recommend_app.api import main
+
+@pytest.fixture(scope='session')
+def testclient():
+    return TestClient( main.app )
+
+#----------------------------------------------------------------------------#
+# Tests
+#----------------------------------------------------------------------------#
+def test_main_root(testclient):
+    response = testclient.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from fastapi101/main"}
